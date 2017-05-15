59887cefb7dd8d0adb313ce5c74a680e87fafc9f
Bogdan <b.bodnarjuk@gmail.com>
2017-05-15 20:37:16 +0300

    Init app

---
+++ app/src/main/java/com/example/bogdan/qrcodeapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.bogdan.qrcodeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends FragmentActivity {
+
+    private LoginButton loginButton;
+
+    private TextView textView;
+
+    private CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        textView = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        textView.setText(
+                                "User ID: "
+                                        + loginResult.getAccessToken().getUserId()
+                                        + "\n" +
+                                        "Auth Token: "
+                                        + loginResult.getAccessToken().getToken()
+                        );
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        textView.setText("Login attempt canceled.");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
